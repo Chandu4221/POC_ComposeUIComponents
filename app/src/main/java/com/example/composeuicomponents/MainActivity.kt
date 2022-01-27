b4f6c39f4f746ff4ecb5115ddbc46073c6ba88cc
dchandra <chndraja@gmail.com>
2022-01-27 12:09:17 +0530

    added custom fonts and typography

--- app/src/main/java/com/example/composeuicomponents/MainActivity.kt
+++ app/src/main/java/com/example/composeuicomponents/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.composeuicomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeuicomponents.ui.theme.ComposeUIComponentsTheme
 
@@ -17,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ComposeUIComponentsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("text")
                 }
             }
         }
@@ -26,13 +30,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column() {
+        Text(text = "h1 $name", style = MaterialTheme.typography.h1)
+        Text(text = "h2 $name", style = MaterialTheme.typography.h2)
+        Text(text = "h3 $name", style = MaterialTheme.typography.h3)
+        Text(text = "h4 $name", style = MaterialTheme.typography.h4)
+        Text(text = "h5 $name", style = MaterialTheme.typography.h5)
+        Text(text = "h6 $name", style = MaterialTheme.typography.h6)
+        Text(text = "Subtitle1 $name", style = MaterialTheme.typography.subtitle1)
+        Text(text = "Subtitle2 $name", style = MaterialTheme.typography.subtitle2)
+        Text(text = "Body1 $name", style = MaterialTheme.typography.body1)
+        Text(text = "Body2 $name", style = MaterialTheme.typography.body2)
+        Text(text = "Button $name", style = MaterialTheme.typography.button)
+        Text(text = "Caption $name", style = MaterialTheme.typography.caption)
+        Text(text = "Overline $name", style = MaterialTheme.typography.overline)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeUIComponentsTheme {
-        Greeting("Android")
+        Greeting("size")
     }
 }
